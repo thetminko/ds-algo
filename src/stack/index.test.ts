9483717abc548681873e71a248fe41e857e9e142
Thet Min Ko <min_ko_thet_from.ufinity@tech.gov.sg>
2025-02-03 13:25:00 +0800

    Init

---
+++ src/stack/index.test.ts
@@ -0,0 +1,11 @@
+import { expect, test } from "vitest";
+import { Stack } from "./index";
+
+test("Stack", () => {
+  expect(new Stack<number>([1, 2, 3]).values()).toStrictEqual([1, 2, 3]);
+  expect(new Stack<number>([1, 2, 3]).size).toBe(3);
+  expect(new Stack<number>([1, 2, 3]).push(4).values()).toStrictEqual([1, 2, 3, 4]);
+  expect(new Stack<number>([1, 2, 3]).pop().values()).toStrictEqual([1, 2]);
+  expect(new Stack<number>([1, 2, 3]).find(2)).toBe(2);
+  expect(new Stack<number>([1, 2, 3]).find(4)).toBe(undefined);
+});
---
+++ src/stack/index.ts
@@ -0,0 +1,30 @@
+export class Stack<T> {
+  private _items: T[] = [];
+
+  constructor(items: T[] = []) {
+    this._items = items;
+  }
+
+  get size() {
+    return this._items.length;
+  }
+
+  push(item: T) {
+    this._items.push(item);
+    return this;
+  }
+
+  pop() {
+    this._items.pop();
+    return this;
+  }
+
+  find(val: T) {
+    return this._items.find(item => item === val);
+  }
+
+  values() {
+    return this._items;
+  }
+
+}
